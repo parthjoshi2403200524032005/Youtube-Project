7df7ce56fc1dccf2873747f52839c5365621e4e0
Parth <parthjohi009@gmail.com>
2024-01-12 11:57:07 +0530

    add4

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+ 
+import { DB_NAME } from "../constants"; 
+
+
+
+const connectDB = async () => {
+    try {
+        
+    } catch (error) {
+        console.log("MONGOOSE ERROR IN CONNECTION",error);
+    }
+}
