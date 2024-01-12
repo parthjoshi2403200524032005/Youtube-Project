a1e1308876d453e939f328f5bf7c819594d005cf
Parth <parthjohi009@gmail.com>
2024-01-12 14:53:35 +0530

    connect to mongodb

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,16 @@
-import mongoose from "mongoose";
- 
-import { DB_NAME } from "../constants"; 
-
-
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
 
 const connectDB = async () => {
-    try {
-        
-    } catch (error) {
-        console.log("MONGOOSE ERROR IN CONNECTION",error);
-    }
-}
+  try {
+    const connectioninstance = await mongoose.connect(
+      `mongodb+srv://Parthyoutube:Parth24032005@cluster0.egs5a3t.mongodb.net/${DB_NAME}`
+    )
+    console.log(`MongoDB Connected: ${connectioninstance.connection.host}`)
+  } catch (error) {
+    console.log("MONGOOSE ERROR IN CONNECTION", error)
+    process.exit(1)
+  }
+} 
+
+export default connectDB ; 
