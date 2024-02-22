51ef8c5031bf90c8d1787278ab91f815d36d7529
Parth <parthjohi009@gmail.com>
2024-02-22 17:21:49 +0530

    add5

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose"
 import { DB_NAME } from "../constants.js"
 
+
 const connectDB = async () => {
   try {
     const connectioninstance = await mongoose.connect(
-      `mongodb+srv://Parthyoutube:Parth24032005@cluster0.egs5a3t.mongodb.net/${DB_NAME}`
+      `mongodb+srv://Parthyoutube:Parth24032005@cluster0.egs5a3t.mongodb.net/`
     )
     console.log(`MongoDB Connected: ${connectioninstance.connection.host}`)
   } catch (error) {
-    console.log("MONGOOSE ERROR IN CONNECTION", error)
+    console.log("MONGOOSE ERROR IN CONNECTION")
     process.exit(1)
   }
 } 
--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ import connectDB from "./db/index.js"
 dotenv.config({ path: "./env" })
 connectDB()
   .then(() => {
-    app.on("error", (err) => {
-      console.log("db connected", err)
+    app.on('error', (err) => {
+      console.log("db connected",err)
     })
-   app.listen(process.env.PORT || 8000, () => {
-       console.log(`server is running on port : ${process.env.PORT}`)
-   })
+    const port = 8001; // Change the port number
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
   })
   .catch((err) => {
     console.log("db not connected",err)
